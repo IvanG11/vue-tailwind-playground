3a8590a518695f6a16df145c5a9aeb4bc632c93b
Iván García <ivangb091@gmail.com>
2020-02-20 00:46:20 +0100

    add roboto font base

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="font-base">
     <router-view />
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        base: ['Roboto', 'sans-serif']
+      }
+    },
   },
   variants: {},
   plugins: [],
