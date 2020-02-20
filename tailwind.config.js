d349e1e7fac30257c18f90c0ab1d0f9c7d32c7ca
Iván García <ivangb091@gmail.com>
2020-02-20 01:08:37 +0100

    change gray color by default

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
     extend: {
       fontFamily: {
         base: ['Roboto', 'sans-serif']
+      },
+      colors: {
+        gray: {
+          '100': '#f7f7f7',
+          '200': '#ededed',
+          '300': '#e2e2e2',
+          '400': '#cbcbcb',
+          '500': '#a0a0a0',
+          '600': '#717171',
+          '700': '#4a4a4a',
+          '800': '#2d2d2d',
+          '900': '#1a1a1a',
+        }
       }
     },
   },
